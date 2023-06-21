78507ad5ed3de275b726dfe6d1338c743970752c
briscx <123821343+briscx@users.noreply.github.com>
2023-06-21 14:08:42 +0200

    Add files via upload

---
+++ Somma.py
@@ -0,0 +1,8 @@
+valore1 = int(input("Inserisci il primo valore intero: "))
+valore2 = int(input("Inserisci il secondo valore intero: "))
+
+somma = valore1 + valore2
+prodotto = valore1 * valore2
+
+print("La somma dei due valori è:", somma)
+print("Il prodotto dei due valori è:", prodotto)
